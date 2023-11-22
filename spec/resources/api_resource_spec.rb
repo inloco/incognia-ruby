ae033ae7f520f0a40f81699472df2bbd7e10e866
Ottony Costa <ottony.costa@incognia.com>
2023-11-22 11:21:32 -0300

    Does not raise error when calling inexistent attribute from ApiResource

--- spec/resources/api_resource_spec.rb
+++ spec/resources/api_resource_spec.rb
@@ -3,18 +3,24 @@ require "spec_helper"
 module Incognia
   RSpec.describe APIResource do
     context ".from_hash" do
-      subject { described_class.from_hash(foo: { bar: :val }, list: [:one, :two]) }
+      subject(:from_hash) { described_class.from_hash(foo: { bar: :val }, list: [:one, :two]) }
 
       it "returns an instance of klass" do
-        expect(subject).to be_instance_of(described_class)
+        expect(from_hash).to be_instance_of(described_class)
       end
 
       it "builds a loose object from a hash" do
-        expect(subject.foo).to respond_to(:bar)
+        expect(from_hash.foo).to respond_to(:bar)
       end
 
       it "builds nested objects" do
-        expect(subject.foo.bar).to eql(:val)
+        expect(from_hash.foo.bar).to eql(:val)
+      end
+
+      context "when calling an inexistent attribute" do
+        it "returns nil" do
+          expect(from_hash.inexistent).to be_nil
+        end
       end
     end
   end
